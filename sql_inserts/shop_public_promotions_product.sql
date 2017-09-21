d267b79e2b1516a1ccd5a75abc9ba9262721a804
lenapy <rashchyk1351@gmail.com>
2017-09-21 20:28:32 +0300

    added  to Git one more sql insert

---
+++ sql_inserts/shop_public_promotions_product.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.promotions_product (id, product_id, promotion_id) VALUES (1, 10, 1);
+INSERT INTO public.promotions_product (id, product_id, promotion_id) VALUES (2, 1, 2);
+INSERT INTO public.promotions_product (id, product_id, promotion_id) VALUES (3, 9, 2);
+INSERT INTO public.promotions_product (id, product_id, promotion_id) VALUES (4, 11, 2);
+INSERT INTO public.promotions_product (id, product_id, promotion_id) VALUES (5, 5, 3);
+INSERT INTO public.promotions_product (id, product_id, promotion_id) VALUES (6, 16, 3);
